a4d0cd7110a1ddcc3552c71cba6bdfb21a5698f5
Iric Gorgi <iricgorgi@gmail.com>
2022-06-04 13:01:04 +0430

    Added navigator status (battery level, online status)

---
+++ public/scripts/navigatordata.js
@@ -0,0 +1,24 @@
+let $ = document;
+let userOnlineStatusElement = $.querySelector(".user-onlinestate");
+let userBatteryLevelDisplay = $.querySelector(".batteryinfo-percentage");
+
+function userOnlineStateDisplay(){
+  let userOnlineStatus = navigator.onLine
+  
+ userOnlineStatus ? userOnlineStatusElement.style.backgroundColor = "#00ff0d" : userOnlineStatusElement.style.backgorundColor = "#ff0000";
+}
+navigator.getBattery()
+  .then(battery =>{
+    updateBatteryDiplay(battery)
+  })
+
+function updateBatteryDiplay(percentage) {
+    let userBatteryPercentage = percentage.level * 100;
+
+    userBatteryLevelDisplay.innerHTML = `${userBatteryPercentage}%`
+    
+}
+
+window.addEventListener("load", ()=>{
+  userOnlineStateDisplay()
+})
