ff62a007352202065b7c649485cfb76a57dbbf19
Iric Gorgi <iricgorgi@gmail.com>
2022-06-06 07:35:01 +0430

    Added some window events and shortcuts

---
+++ public/scripts/index.js
@@ -0,0 +1,22 @@
+"use strict"
+
+import { dateAtTheMomment , userOnlineStateDisplay , _uuid , updateBatteryDiplay } from "./massageinfo.js"
+
+let $ = document;
+const massageInput = $.querySelector(".chat-massage-input");
+const massageSubmitBtn = $.querySelector(".chat-massage-submit");
+const massageContainer = $.querySelector(".chat-massage-container");
+const massageForm = $.querySelector(".chat-massage-form");
+
+massageForm.addEventListener("click", (e)=>{
+  e.preventDefault();
+})
+window.addEventListener("keypress", (e)=>{
+  if (e.key === "enter"){
+    newMassageGenerator()
+  }
+})
+window.addEventListener("load", ()=>{
+  userOnlineStateDisplay()
+  updateBatteryDiplay()
+})
