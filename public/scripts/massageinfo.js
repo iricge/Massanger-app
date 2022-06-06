1a667e9a6783a2739416da517adb97583b529d98
Iric Gorgi <iricgorgi@gmail.com>
2022-06-06 07:33:20 +0430

    Added a function that gets user battery perentage and updates it in DOM

--- public/scripts/massageinfo.js
+++ public/scripts/massageinfo.js
@@ -24,3 +24,12 @@ export function userOnlineStateDisplay(){
   let userOnlineStatus = navigator.onLine
   userOnlineStatus ? userOnlineStatusElement.style.backgroundColor = "#00ff0d" : userOnlineStatusElement.style.backgorundColor = "#ff0000";
 }
+//! ---------------------- Gets users battery percentage and gives to a function that updates the DOM ---------------------- !\\
+export function updateBatteryDiplay() {
+  navigator.getBattery()
+  .then(battery =>{
+    let userBatteryPercentage = battery.level * 100
+    userBatteryLevelDisplay.innerHTML = `${userBatteryPercentage}%`
+    console.log(userBatteryPercentage);
+  })
+}
